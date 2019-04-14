e9f0dfedc55f8cebb125f32e1ca9ef0b6e5bb0dd
MAJU STUDENT <majustudent@jinnah.edu>
2019-04-14 16:00:43 +0500

    2nd push

--- src/resume.js
+++ src/resume.js
@@ -1,5 +1,12 @@
 import React, { Component } from 'react';
-import { Container, Header,  Content, FooterTab, Button, Text } from 'native-base';
+import { Container, Header,  Content, FooterTab, Button, Text, Accordion  } from 'native-base';
+
+const dataArray = [
+    { title: "Objective", content: "My objective is to fullfill the requirements of organization and do the best I can" },
+    { title: "Experience", content: "Worked as freelancer on Upwork from 2012\n Worked as freelancer on Fiverr from 2012." },
+    { title: "Education", content: "Matric 2005\n Inter 2007\n BSc. 2010\n MCS In progress" }
+  ];
+
 
 export default class GeneralExample extends Component {
   render() {
@@ -13,6 +20,10 @@ export default class GeneralExample extends Component {
           </Body>
           <Right />
         </Header>
+
+
+
+
 
 
         <Footer>
